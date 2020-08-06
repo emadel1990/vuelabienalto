6c80f26501969adb315c21daf3cac006d7f0b3a0
Gabriel <gbritez53@gmail.com>
2020-08-06 13:51:19 -0300

    Elimino la carpeta assets

---
+++ src/components/constantes.js
@@ -0,0 +1,33 @@
+const uri = "http://drive.google.com/uc?export=view&id=";
+
+//logoNav
+export const logoNav = `${uri}12FEYse3SvPwGN25fpcyYmdSBdYrHEkBj`;
+
+//video Header
+export const portada = `${uri}1PbKP7gqTNEKHv2K9qZzq1mkLIri5XNi1`;
+
+//video home
+export const videoDirector = `${uri}1JB1B3ikihMmP4YvJHohNCH8UlnViHx3B`;
+
+//team
+export const Claudio = `${uri}1m5c6m2yIPho89rAtBSEXbsdSH_r9Mrql`;
+export const Yani = `${uri}1WpT7sleVukzPHXOQNmAieWH0gb28Bg3l`;
+export const Ariel = `${uri}16m9mQSA_t6DzJm7qHUHtgDhR3Qtt75k0`;
+export const Vivian = `${uri}1KZeDVrGuaq22sXs9OuTAZ1bKq702vmwu`;
+export const Gloria = `${uri}1oNBA-laNC6fcaSSmOzSUVU7G5oVQAxcw`;
+export const Pedro = `${uri}1c3YHF_DpLCF5CavmxSxGdDHETYeWlao4`;
+
+//Courses
+export const PNL = `${uri}10A9njTa4PzKvk-g140u8Hf4-nUZmYRVM`;
+export const ConstFamiliares = `${uri}1Y8vMU6NTJE6GRqsxa8_ZfVVuPpW7MkS5`;
+export const Coaching = `${uri}1T4ogADLW8hRxRp71Qp60JrWQ-Ts3sZ-g`;
+export const Oratoria =
+  "https://cgcesantafe.org.ar/wp-content/uploads/2017/10/oratoria-y-comunicacion.jpg";
+
+//svg
+export const check = "https://image.flaticon.com/icons/svg/929/929440.svg";
+export const goal = "https://image.flaticon.com/icons/svg/3214/3214721.svg";
+
+//others
+export const pensando =
+  "https://lh3.googleusercontent.com/proxy/LeZwsChOGscV6DMEH9SnQDsjPgDBvz6dVybzw_k9iJvyMnQkRxrP2wY3ZM4NdjRdyY1e_hM1pBeseZG-HvUQuONgylV3qAztzmy9vuHwlYUkCPJa6T83ARL4kjjw6UA";
